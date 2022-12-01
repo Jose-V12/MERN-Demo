7758b957ec20f9147bc7d309faf9bfc3bab55bae
Jose-V12 <jv123144@gmail.com>
2022-12-01 11:52:09 -0600

    Incoporated Routes to Server

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
+const router = require("./Routes/Store-Routes");
+app.use("/Schnucks", router);
 mongoose.connect("mongodb+srv://Jose-V:Josevargas123144!@cluster0.qwyfktd.mongodb.net/Schnuks?retryWrites=true&w=majority"
 ).then(() => console.log("Connected To Database"))
 .then(() => {
